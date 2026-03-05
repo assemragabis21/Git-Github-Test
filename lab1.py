42111bf62df302c1ab521b05b1f9cff8de42e8fa
MarsXc0 <assemragab105@gmail.com>
2026-03-05 14:00:05 +0200

    initial commit

---
+++ lab1.py
@@ -0,0 +1,21 @@
+# Lab 1 - Git Practice
+# Name: Assem Ragab Mohamed Mahmoud Ebrahim
+
+def main():
+    print("Git Lab 1 Practice")
+
+    # Commit 1
+    print("First commit change")
+
+    # Commit 2
+    print("Second commit change")
+
+    # Commit 3
+    print("Third commit change")
+
+    # Commit 4
+    print("Fourth commit change")
+
+
+if __name__ == "__main__":
+    main()
