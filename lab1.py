f669848749b4bccbfd2f29438433efbbbbb82687
MarsXc0 <assemragab105@gmail.com>
2026-03-05 14:49:47 +0200

    Revert "finish"
    This reverts commit a16841fcc4c1f7c56423c5db8e3943b8dedf1c91.

--- lab1.py
+++ lab1.py
@@ -17,8 +17,8 @@ def main():
     # Commit 4
     print("Fourth commit change")
 
-    print("commit after deletion")
-    print("lab done")
+	print("commit after deletion")
+	print("lab done")
 
 if __name__ == "__main__":
     main()
